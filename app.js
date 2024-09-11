bd2937673e8e1d4753a9f6803efd99932ca70619
Hamzasohail123 <hamzasohail429@gmail.com>
2024-09-11 12:34:44 +0500

    Etch a Skecth App using JavaScript

---
+++ app.js
@@ -0,0 +1,88 @@
+let click = true;
+let board = document.getElementById("board");
+let square = board.querySelectorAll("div");
+let eraser = document.getElementById("eraser");
+let clear = document.getElementById("clear");
+let random = document.getElementById("rainbowMode");
+let sizeValue = document.getElementById("sizeValue");
+let colorPicker = document.getElementById("colorPicker");
+let colorMode = document.getElementById("colorMode");
+function boardCreation(size) {
+  // square.forEach((div) => div.remove(div));
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  console.log(board);
+  sizeValue.innerHTML = `${size} x ${size}`;
+
+  let totalSize = size * size;
+  for (let i = 0; i < totalSize; i++) {
+    const div = document.createElement("div");
+    // square.style.backgroundColor = "blue"; // Initial color
+    div.addEventListener("mouseover", () => colorSquare(div, "black"));
+    board.appendChild(div);
+  }
+}
+
+// Color Picker
+colorMode.addEventListener("click", function (e) {
+  let colorVal = colorPicker.value;
+  board.querySelectorAll("div").forEach((div) => {
+    colorMode.style.backgroundColor = "black";
+    colorMode.style.color = "white";
+    div.addEventListener("mouseover", () => colorSquare(div, colorVal));
+  });
+});
+
+// Eraser Handeler
+eraser.addEventListener("click", function () {
+  eraser.style.backgroundColor = "black";
+  eraser.style.color = "white";
+  board.querySelectorAll("div").forEach((div) => {
+    div.addEventListener("mouseover", () => colorSquare(div, "white"));
+  });
+});
+
+// Random Color
+random.addEventListener("click", function () {
+  random.style.backgroundColor = "black";
+  random.style.color = "white";
+  board.querySelectorAll("div").forEach((div) => {
+    div.addEventListener("mouseover", () => colorSquare(div, "random"));
+  });
+});
+
+// Clear All
+clear.addEventListener("click", () => {
+  board
+    .querySelectorAll("div")
+    .forEach((div) => (div.style.backgroundColor = "white"));
+});
+
+// Add event listener to the range input
+document.addEventListener("DOMContentLoaded", () => {
+  const sizeRange = document.getElementById("sizeRange");
+  boardCreation(sizeRange.value); //  initial value of range
+  sizeRange.addEventListener("input", (event) => {
+    board.querySelectorAll("div").forEach((div) => {
+      div.style.backgroundColor = "white";
+    });
+    const value = event.target.value;
+    console.log("eventttttttttt", event.target.value);
+    boardCreation(value);
+  });
+});
+
+function colorSquare(elem, color) {
+  if (click) {
+    if (color === "random") {
+      const hue = Math.floor(Math.random() * 360);
+      elem.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+    } else {
+      elem.style.backgroundColor = color;
+    }
+  }
+}
+
+document.querySelector("body").addEventListener("click", () => {
+  click = !click;
+});
